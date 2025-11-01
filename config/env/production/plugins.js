ff3f364c23a4349645dbe7e335eb447884ecaa25
unknown <juliusz.czechowski@gmail.com>
2025-11-01 22:43:29 +0100

    ponowna poprawa konfiguracji

--- config/env/production/plugins.js
+++ config/env/production/plugins.js
@@ -1,18 +1,13 @@
-// config/plugins.js
-
+// config/env/production/plugins.js
 module.exports = ({ env }) => ({
-  // ... other plugin configurations if you have any ...
-
   email: {
     config: {
-      provider: "sendgrid", // Use the SendGrid provider
+      provider: "sendgrid",
       providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"), // Your API key from the .env file
+        apiKey: env("SENDGRID_API_KEY"),
       },
       settings: {
-        // Ensure this email is verified as a sender in SendGrid
         defaultFrom: env("SENDGRID_DEFAULT_FROM", "kontakt@startweb.com.pl"),
-        // Email address replies should go to
         defaultReplyTo: env(
           "SENDGRID_DEFAULT_REPLY_TO",
           "kontakt@startweb.com.pl"
@@ -20,6 +15,4 @@ module.exports = ({ env }) => ({
       },
     },
   },
-
-  // ... other configurations ...
 });
--- config/plugins.js
+++ config/plugins.js
@@ -2,24 +2,4 @@
 
 module.exports = ({ env }) => ({
   // ... other plugin configurations if you have any ...
-
-  email: {
-    config: {
-      provider: "sendgrid", // Use the SendGrid provider
-      providerOptions: {
-        apiKey: env("SENDGRID_API_KEY"), // Your API key from the .env file
-      },
-      settings: {
-        // Ensure this email is verified as a sender in SendGrid
-        defaultFrom: env("SENDGRID_DEFAULT_FROM", "kontakt@startweb.com.pl"),
-        // Email address replies should go to
-        defaultReplyTo: env(
-          "SENDGRID_DEFAULT_REPLY_TO",
-          "kontakt@startweb.com.pl"
-        ),
-      },
-    },
-  },
-
-  // ... other configurations ...
 });
